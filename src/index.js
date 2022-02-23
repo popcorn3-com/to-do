acb8af1e56ed4f035cbb7f49757a98a3f51b3f05
Hiro <sgi78ks@popcorn3.com>
2022-02-24 08:13:27 +0900

    add li

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import "./styles.css";
 
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  // liを生成
+  const li = document.createElement("li");
+  li.classList.add("list-item");
+
+  // pタグを生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を追加
+  li.appendChild(p);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+  console.log(li);
 };
 
 document
